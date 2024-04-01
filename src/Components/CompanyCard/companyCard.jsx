b849311c90dd7fca36bf65ad38c09a898e18f649
umairakramspsi <136310241+umairakramspsi@users.noreply.github.com>
2024-04-01 13:53:07 +0500

    updatedCode-1April

---
+++ src/Components/CompanyCard/companyCard.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const CompanyCard = (props) => {
+
+    console.log(props)
+
+  return (
+    <div>
+      <div className="" style={{backgroundImage: `url(${props.image.src})`,backgroundRepeat:"no-repeat",backgroundSize:'100%'}}>
+        <div className="company-name">
+            <p className=''>{props.companyName}</p></div>
+      </div>
+    </div>
+  )
+}
+
+export default CompanyCard
